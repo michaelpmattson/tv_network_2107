5f1df604c0aac82f85137a9d15b8a68f1e28d3f9
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-21 09:35:53 -0600

    Add add_show

--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -17,33 +17,30 @@ RSpec.describe Network do
       expect(nbc.shows).to eq([])
     end
   end
+
+  context '#add_show(show)' do
+    it 'adds a show' do
+      nbc = Network.new("NBC")
+
+      michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+      kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+      knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+      leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+      ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+      parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+      expect(nbc.shows).to eq([])
+
+      nbc.add_show(knight_rider)
+      nbc.add_show(parks_and_rec)
+
+      expect(nbc.shows).to eq([knight_rider, parks_and_rec])
+    end
+  end
 end
 
-# michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
-# # => #<Character:0x00007fe5f88721f8...>
-#
-# kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
-# # => #<Character:0x00007fe5f8448f78...>
-#
-# knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
-# # => #<Show:0x00007fe5f8398970...>
-#
-# leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
-# # => #<Character:0x00007fe5f832bb18...>
-#
-# ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
-# # => #<Character:0x00007fe5f8172a60...>
-#
-# parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
-# # => #<Show:0x00007fe5f88b0a20...>
-#
-# nbc.add_show(knight_rider)
-#
-# nbc.add_show(parks_and_rec)
-#
-# nbc.shows
-# # => [#<Show:0x00007fe5f8398970...>, #<Show:0x00007fe5f88b0a20...>]
-#
+
 # nbc.main_characters
 # # => [#<Character:0x00007f98a4ba8dc8...>]
 #
