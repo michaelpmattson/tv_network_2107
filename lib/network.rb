05266b27550a11a33a52a4a56e67320ce1c1ee71
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-21 09:54:54 -0600

    Add main_characters

--- lib/network.rb
+++ lib/network.rb
@@ -12,6 +12,16 @@ class Network
   end
 
   def main_characters
+    main = []
+    characters.each do |character|
+      main << character if character.bankroll? && character.upcase?
+    end
+    main
+  end
 
+  def characters
+    shows.flat_map do |show|
+      show.characters
+    end
   end
 end
--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe Network do
   end
 
   context 'main_characters' do
-    xit 'returns characters making 500_000 and in UPCASE' do
+    it 'returns characters making 500_000 and in UPCASE' do
       nbc = Network.new("NBC")
 
       michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
@@ -55,6 +55,25 @@ RSpec.describe Network do
       nbc.add_show(parks_and_rec)
 
       expect(nbc.main_characters).to eq([kitt])
+    end
+  end
+
+  context 'characters' do
+    it 'returns all characters' do
+      nbc = Network.new("NBC")
+
+      michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+      kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+      knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+      leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+      ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+      parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+      nbc.add_show(knight_rider)
+      nbc.add_show(parks_and_rec)
+
+      expect(nbc.characters).to eq([michael_knight, kitt, leslie_knope, ron_swanson])
     end
   end
 end
